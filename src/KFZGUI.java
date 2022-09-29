51b874a0683c07105efd6a94d49bdf0cbd8cc6e2
Tim <tim.steuerr@gmail.com>
2022-09-29 21:28:35 +0200

    s

--- src/KFZGUI.java
+++ src/KFZGUI.java
@@ -1,9 +1,21 @@
 import javax.swing.*;
 
 public class KFZGUI extends JFrame {
-    private JLabel kfzpruefer;
+    private JLabel Jkfzpruefer;
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JLabel Jkennzeichen;
+    private JButton pruefeButton;
 
     public KFZGUI(){
+
+     setVisible(true);
+     setTitle("KFZGUI");
+     setSize(400,400);
+     setResizable(true);
+     Jkfzpruefer.setVisible(true);
+     setContentPane(mainPanel);
+     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
 }
