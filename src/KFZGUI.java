c0a6997b54b084836158c4916baa8180cfd43cc3
Tim <tim.steuerr@gmail.com>
2022-09-30 17:38:20 +0200

    30.09.22

--- src/KFZGUI.java
+++ src/KFZGUI.java
@@ -1,21 +1,21 @@
 import javax.swing.*;
 
 public class KFZGUI extends JFrame {
-    private JLabel Jkfzpruefer;
+    private JLabel Jkfzpruefer;  //Inhalte der Gui werden deklariert
     private JPanel mainPanel;
     private JTextField textField1;
     private JLabel Jkennzeichen;
     private JButton pruefeButton;
 
     public KFZGUI(){
-
      setVisible(true);
      setTitle("KFZGUI");
-     setSize(400,400);
+     setSize(300,180);
      setResizable(true);
      Jkfzpruefer.setVisible(true);
      setContentPane(mainPanel);
      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+     KFZPruefer p = new KFZPruefer();        //KFZPruefer wird erstellt
 
     }
 }
--- src/KFZPruefer.java
+++ src/KFZPruefer.java
@@ -1,2 +1,32 @@
 public class KFZPruefer {
+    private static int zustand = 0;
+    private static boolean correct;
+    public KFZPruefer(){
+
+    }
+    public static void zustandWechseln(char pZeichen){
+
+
+        switch(zustand){
+          case 0: {
+              switch(pZeichen) {
+                  case : {zustand = 1;} break;
+                  case 'B': {zustand = 0;} break;
+              }
+          }break;
+
+      }
+    }
+
+    public static boolean wortUntersuchen(String pEingabe){
+             correct =false;
+            for(int i = 0; i<pEingabe.length(); i++){    //Jeden Buchstaben einzeln Testen
+                zustandWechseln(pEingabe.charAt(i));
+            }
+            if(zustand ==10){              //Wenn alle Zustände druchgegagnen sind muss das Kennzeichen korrekt sein
+                correct = true;
+            }
+        return correct;   //Ergebnis der Prüfung wird zurück gegeben
+    }
+
 }
